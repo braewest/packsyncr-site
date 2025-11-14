810aeb197fc2467ac9ad0a56d3f0d66604b1a2e6
Braeden West <braedenwest1@gmail.com>
2025-11-14 11:54:50 -0700

    Moved authorization to Azure
    The Mojang authorization API call was blocking the request from Cloudflare, so I am migrating the authorization to Azure.

--- docs/js/callback.js
+++ docs/js/callback.js
@@ -1,4 +1,4 @@
-const AUTH_PROXY_URL = "https://auth-proxy.packsyncr.workers.dev/";
+const AUTH_PROXY_URL = "https://packsyncr-auth-bteqekhzbkfaegd6.canadacentral-01.azurewebsites.net/api/auth-proxy";
 
 document.addEventListener("DOMContentLoaded", async () => {
     // Get auth code and state from url
